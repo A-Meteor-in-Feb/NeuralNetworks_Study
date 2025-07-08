9780423b26f3fa6e54802c9db3a4f63ed5de9802
A-Meteor-in-Feb <yang.diandian@hotmail.com>
2025-07-08 14:22:54 +0800

    data classification

--- tabularData_classification.py
+++ tabularData_classification.py
@@ -155,3 +155,126 @@ model = MyModel().to(device)
 #loss function (it's named criterion in most references)
 criterion = nn.BCELoss() # BCELoss is the Binary Cross-Entropy for binary classification
 optimizer = Adam(model.parameters(), lr=1e-3)
+
+total_loss_train_plot = []
+total_loss_validation_plot = []
+total_acc_train_plot = []
+total_acc_validation_plot = []
+
+epochs = 10
+
+for epoch in range(epochs):
+    total_acc_train = 0
+    total_loss_train = 0
+    total_acc_val = 0
+    total_loss_val = 0
+
+    for data in train_dataloader:
+        inputs, labels = data
+        # get the ouput from the model
+        prediction = model(inputs).squeeze(1)
+
+        #print(prediction.shape)            #the output is torch.Size([8, 1]) 就是有8 rows
+        #print(prediction.squeeze(1).shape) #the output is torch.Size([8]). 就是有 8 columns
+        
+        batch_loss = criterion(prediction, labels)
+        total_loss_train += batch_loss.item()
+
+        #the output be like: tensor([False, True, False, True, False, False, True, True], device='mps')
+        #because we want to know the accuracy, so sum
+        #after sum, we will get "tensor(3, device=...)", which means we get 3 True out of 8
+        #item will give you the only number -- 3
+        #the batch's accuracy
+        acc = ((prediction).round() == labels).sum().item()
+        total_acc_train += acc
+
+        #backpropagation
+        batch_loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+
+    # this means you use the pytorch model not for training.
+    # Just for testing. 
+    with torch.no_grad():
+        for data in validation_dataloader:
+            inputs, labels = data
+            prediction = model(inputs).squeeze(1)
+            batch_loss = criterion(prediction, labels)
+            total_loss_val += batch_loss.item()
+            acc = ((prediction).round() == labels).sum().item()
+            total_acc_val += acc
+
+    # divide by 1000 to do normalization.
+    total_loss_train_plot.append(round(total_loss_train/1000, 4))
+    total_loss_validation_plot.append(round(total_loss_val/1000, 4))
+
+    #calculate the precentage
+    # __len__() give us the number of data set.
+    total_acc_train_plot.append(round(total_acc_train/(training_data.__len__()) * 100, 4))
+    total_acc_validation_plot.append(round(total_acc_val/(validation_data.__len__()) * 100, 4))
+
+    print(f'''Epoch no. {epoch + 1} Train loss: {round(total_acc_train/1000, 4)} Train accuracy {round(total_acc_train/training_data.__len__() * 100, 4)}
+            Validation Loss: {round(total_loss_val/1000, 4)} Validation Accuracy: {round(total_acc_val/validation_data.__len__() * 100, 4)}''')
+    print('=' * 25)
+    
+
+    #do testing
+    with torch.no_grad():
+        total_loss_test = 0
+        total_acc_test = 0
+        for data in testing_dataloader:
+            inputs, label = data
+            prediction = model(inputs).squeeze(1)
+            batch_loss_test = criterion(prediction, labels).item()
+            total_loss_test += batch_loss_test
+            acc = ((prediction).round() == labels).sum().item()
+            total_acc_test += acc
+
+    print("Accuracy: ", round(total_acc_test/testing_data.__len__() * 100, 4) )
+
+#visualization
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+axs[0].plot(total_loss_train_plot, label='Training Loss')
+axs[0].plot(total_loss_validation_plot, label='Validation Loss')
+axs[0].set_title('Training and Validation Loss over Epochs')
+axs[0].set_xlabel('Epochs')
+axs[0].set_ylabel('Loss')
+axs[0].set_ylim([0, 2])
+axs[0].legend()
+
+axs[1].plot(total_acc_train_plot, label='Training Accuracy')
+axs[1].plot(total_acc_validation_plot, label='Validation Accuracy')
+axs[1].set_title('Training and Validation Accuracy over Epochs')
+axs[1].set_xlabel('Epochs')
+axs[1].set_ylabel('Accuracy')
+axs[1].set_ylim([0, 100])
+axs[1].legend()
+
+plt.tight_layout()
+
+plt.show()
+
+
+#inference
+
+area = float(input("Area: "))/original_df['Area'].abs().max()
+MajorAxisLength = float(input("Major Axis Length: "))/original_df['MajorAxisLength'].abs().max()
+MinorAxisLength = float(input("Minor Axis Length: "))/original_df['MinorAxisLength'].abs().max()
+Eccentricity = float(input("Eccentricity: "))/original_df['Eccentricity'].abs().max()
+ConvexArea = float(input("Convex Area: "))/original_df['ConvexArea'].abs().max()
+EquivDiameter = float(input("EquivDiameter: "))/original_df['EquivDiameter'].abs().max()
+Extent = float(input("Extent: "))/original_df['Extent'].abs().max()
+Perimeter = float(input("Perimeter: "))/original_df['Perimeter'].abs().max()
+Roundness = float(input("Roundness: "))/original_df['Roundness'].abs().max()
+AspectRation = float(input("AspectRation: "))/original_df['AspectRation'].abs().max()
+
+my_inputs = [area, MajorAxisLength, MinorAxisLength, Eccentricity, ConvexArea, EquivDiameter, Extent, Perimeter, Roundness, AspectRation]
+
+print("="*20)
+
+model_inputs = torch.Tensor(my_inputs).to(device)
+prediction = (model(model_inputs))
+print(prediction)
+print("Class is: ", round(prediction.item()))
+     
